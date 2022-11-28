8b86fbf70656d501dc6d93123f2c9e183b22ddc8
ashwin <ashwin.ramki96@gmail.com>
2022-11-28 16:24:21 -0600

    made changes to models and migration file

--- app/models/other_models.py
+++ app/models/other_models.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .user import User
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -12,12 +12,12 @@ subreddits_users = db.Table(
     db.Column(
         "subreddit_id",
         db.Integer,
-        db.ForeignKey("subreddits.id")
+        db.ForeignKey(add_prefix_for_prod("subreddits.id"))
     ),
     db.Column(
         "user_id",
         db.Integer,
-        db.ForeignKey("users.id")
+        db.ForeignKey(add_prefix_for_prod("users.id"))
     )
 )
 
@@ -30,7 +30,7 @@ class Subreddit(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     about = db.Column(db.String(10000), nullable = False)
-    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable = False)
     created_at = db.Column(db.DateTime, nullable=False, unique=False, index=False, default=datetime.now)
 
     posts = db.relationship("Post", back_populates = "subreddit", cascade = "all, delete-orphan")
@@ -55,8 +55,8 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(2000), nullable = False)
     text = db.Column(db.String(2000), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    subreddit_id = db.Column(db.Integer, db.ForeignKey("subreddits.id"), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable = False)
+    subreddit_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("subreddits.id")), nullable = False)
     created_at = db.Column(db.DateTime, nullable=False, unique=False, index=False, default=datetime.now)
     user = db.relationship("User", back_populates = "posts")
     comments = db.relationship("Comment", back_populates = "post", cascade = "all, delete-orphan")
@@ -82,8 +82,8 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     comment = db.Column(db.String(10000), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable = False)
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("posts.id")), nullable = False)
     created_at = db.Column(db.DateTime, nullable=False, unique=False, index=False, default=datetime.now)
     user = db.relationship("User", back_populates = "comments")
     post = db.relationship("Post", back_populates = "comments")
--- migrations/versions/484347f23f17_.py
+++ migrations/versions/484347f23f17_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 5eff9eb8e6df
+Revision ID: 484347f23f17
 Revises: 
-Create Date: 2022-11-25 15:19:34.925003
+Create Date: 2022-11-28 16:22:20.951298
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5eff9eb8e6df'
+revision = '484347f23f17'
 down_revision = None
 branch_labels = None
 depends_on = None
